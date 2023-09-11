3b4afb2045d0d84e7870c3ba5b6f7be469be1a65
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-09-11 19:47:15 +0300

    Add Startup class

---
+++ FileUploadTrigger/FileUploadTrigger/Startup.cs
@@ -0,0 +1,21 @@
+﻿using FileUploadTrigger;
+using FileUploadTrigger.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using SendGrid.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace FileUploadTrigger
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+            builder.Services.AddSendGrid(options =>
+                options.ApiKey = builder.GetContext().Configuration["SendGridApiKey"]);
+
+            builder.Services.AddTransient<IEmailSender, EmailSender>();
+        }
+    }
+}

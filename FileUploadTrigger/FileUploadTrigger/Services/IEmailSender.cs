707acbb1a697bccb726309a64f8696c512ac62d1
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-09-11 19:42:22 +0300

    Add IEmailSender interface

---
+++ FileUploadTrigger/FileUploadTrigger/Services/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using FileUploadTrigger.Models;
+using System.Threading.Tasks;
+
+namespace FileUploadTrigger.Services
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendAsync(EmailOptions options);
+    }
+}

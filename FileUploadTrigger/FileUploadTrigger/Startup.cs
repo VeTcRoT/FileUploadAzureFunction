dcda315cd15e0441905d678d50826e1f07f912d2
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-09-11 19:53:15 +0300

    Implement IBlobService

--- FileUploadTrigger/FileUploadTrigger/Startup.cs
+++ FileUploadTrigger/FileUploadTrigger/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using FileUploadTrigger;
 using FileUploadTrigger.Services;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
 using SendGrid.Extensions.DependencyInjection;
 
@@ -11,11 +12,18 @@ namespace FileUploadTrigger
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            builder.Services.AddAzureClients(clientBuilder =>
+            {
+                clientBuilder
+                    .AddBlobServiceClient(builder.GetContext().Configuration["BlobConnectionString"]);
+            });
 
             builder.Services.AddSendGrid(options =>
                 options.ApiKey = builder.GetContext().Configuration["SendGridApiKey"]);
 
             builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+            builder.Services.AddTransient<IBlobService, BlobService>();
         }
     }
 }

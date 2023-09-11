6dcdf86dea43baae3d1a157b6fe17fde96a88bae
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-09-11 19:41:35 +0300

    Add EmailOptions model

---
+++ FileUploadTrigger/FileUploadTrigger/Models/EmailOptions.cs
@@ -0,0 +1,20 @@
+﻿using SendGrid.Helpers.Mail;
+
+namespace FileUploadTrigger.Models
+{
+    public class EmailOptions
+    {
+        public EmailAddress From { get; set; }
+        public EmailAddress To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public EmailOptions(string from, string to, string subject, string body)
+        {
+            From = new(from);
+            To = new(to);
+            Subject = subject;
+            Body = body;
+        }
+    }
+}
